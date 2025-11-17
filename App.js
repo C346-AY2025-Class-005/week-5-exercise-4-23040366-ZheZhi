e56ec32ffd09778e810e4ba855810c777e97a2b9
Zhe <hwazhezhi@gmail.com>
2025-11-17 10:29:25 +0800

    Week 5 Submission 1 commit 1

--- App.js
+++ App.js
@@ -1,20 +1,68 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, SectionList, TouchableOpacity } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>This is a Test</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const datasource = [
+    {data: [
+            {key: 'a'},
+            {key: 'e'},
+            {key: 'i'},
+            {key: 'o'},
+            {key: 'u'}
+        ], title: "Vowels", bgcolor: 'skyblue'},
+    {data: [
+            {key: 'b'},
+            {key: 'c'},
+            {key: 'd'},
+            {key: 'f'},
+            {key: 'g'},
+            {key: 'h'},
+            {key: 'j'},
+            {key: 'k'},
+            {key: 'l'},
+            {key: 'm'},
+            {key: 'n'},
+            {key: 'p'},
+            {key: 'q'}
+        ], title: "Consonants", bgcolor: 'khaki'}
+];
+
+const renderItem = ({item}) => {
+    return(
+        <TouchableOpacity style={styles.opacityStyle} >
+            <Text style={styles.textStyle} >{item.key}</Text>;
+        </TouchableOpacity>
+    );
+};
+
+const App = () => {
+    return (
+        <View>
+            <SectionList
+                contentContainerStyle = {{padding: 10}}
+                sections={datasource}
+                renderItem ={renderItem}
+                renderSectionHeader = {({section:{title, bgcolor}}) => (
+                    <Text style={[styles.headertext, {backgroundColor: bgcolor}]}>{title}</Text>
+                )}/>
+        </View>
+    );
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    textStyle: {
+        fontSize: 15,
+        margin: 10,
+        textAlign: 'left'
+    },
+    opacityStyle: {
+        borderWidth: 1
+    },
+    headertext: {
+        fontSize: 20,
+        margin: 10,
+        textAlign: 'center',
+        fontWeight: 'bold'
+    },
 });
+
+export default App;
